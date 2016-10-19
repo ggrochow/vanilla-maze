7d0cc8f554ea6d91326a32eff13129d999e6c203
Graham Grochowski <ggrochow@gmail.com>
2016-10-18 18:06:36 -0700

    add generate function to create new mazes on the fly

--- src/board.js
+++ src/board.js
@@ -2,72 +2,62 @@ import Cell from "./cell";
 import {sample} from "./utils";
 
 class Board {
-  constructor(rows = 10, cols = 10) {
-    this.rows = rows;
-    this.cols = cols;
-  }
+    constructor(rows = 10, cols = 10) {
+        this.rows = rows;
+        this.cols = cols;
 
-  size() {
-    return this.rows * this.cols;
-  }
-
-  prepare() {
-    this.grid = [];
-    for (let row = 0; row < this.rows; row++) {
-      const rowArray = [];
-      for (let col = 0; col < this.cols; col++) {
-        rowArray[col] = new Cell(row, col);
-      }
-      this.grid[row] = rowArray;
+        this.grid = [];
+        for (let row = 0; row < this.rows; row++) {
+            const rowArray = [];
+            for (let col = 0; col < this.cols; col++) {
+                rowArray[col] = new Cell(row, col);
+            }
+            this.grid[row] = rowArray;
+        }
     }
-  }
 
-  insertObject(object) {
-    const cell = this.randomCell();
-    cell.contents = object;
-    object.cell = cell;
-  }
-
-  placeExit(object) {
-    // TODO: random placement.
-    const cell = this.get(0, 0);
-    cell.contents = object;
-    object.cell = cell;
-  }
-
-  get(row, col) {
-    if ((row >= this.rows || row < 0) || (col >= this.cols || col < 0)) {
-      return;
+    insertObject(object) {
+        const cell = this.randomCell();
+        cell.contents = object;
+        object.cell = cell;
     }
-    return this.grid[row][col];
-  }
 
-  configureCells() {
-    this.eachCell(cell => {
-      const row = cell.y;
-      const col = cell.x;
+    placeExit(object) {
+        // TODO: random placement.
+        const cell = this.get(0, 0);
+        cell.contents = object;
+        object.cell = cell;
+    }
 
-      cell.north = this.get(row - 1, col);
-      cell.south = this.get(row + 1, col);
-      cell.west = this.get(row, col - 1);
-      cell.east = this.get(row, col + 1);
-    });
-  }
+    get(row, col) {
+        if ((row >= this.rows || row < 0) || (col >= this.cols || col < 0)) {
+            return;
+        }
+        return this.grid[row][col];
+    }
 
-  eachCell(cb) {
-    this.grid.forEach(row => {
-      row.some(cell => cb(cell));
-    });
-  }
+    configureCells() {
+        this.eachCell(cell => {
+            const row = cell.y;
+            const col = cell.x;
 
-  eachRow(cb) {
-    this.grid.forEach(row => cb(row));
-  }
+            cell.north = this.get(row - 1, col);
+            cell.south = this.get(row + 1, col);
+            cell.west = this.get(row, col - 1);
+            cell.east = this.get(row, col + 1);
+        });
+    }
 
-  randomCell() {
-    const randomRow = sample(this.grid);
-    return sample(randomRow);
-  }
+    eachCell(cb) {
+        this.grid.forEach(row => {
+            row.some(cell => cb(cell));
+        });
+    }
+
+    randomCell() {
+        const randomRow = sample(this.grid);
+        return sample(randomRow);
+    }
 }
 
 
--- src/index.js
+++ src/index.js
@@ -6,23 +6,17 @@ import huntAndKill from "./hunt_and_kill.js";
 import drawBoardToCanvas from "./canvas_presenter.js";
 import {CELL_SIZE} from "./constants.js";
 import {genSolve} from "./dijkstra.js";
-const board = new Board(25, 25);
+
+let board = new Board(25, 25);
 const player = new Player();
 const exit = new Exit();
 
-board.prepare();
-board.configureCells();
-board.insertObject(player);
-board.placeExit(exit);
-
-huntAndKill(board);
 
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('canvas');
     canvas.height = board.rows * CELL_SIZE;
     canvas.width = board.cols * CELL_SIZE;
-
-    drawBoardToCanvas(board);
+    setupNewMaze();
 
     document.addEventListener('keydown', e => {
         switch (e.keyCode) {
@@ -54,7 +48,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const gen = genSolve(player.cell, exit.cell);
         animateSolve(gen, board);
     });
+
+    const newButton = document.getElementById('newButton');
+    newButton.addEventListener('click', setupNewMaze);
 });
+
+function setupNewMaze() {
+    board = new Board(25, 25);
+    board.configureCells();
+    board.insertObject(player);
+    board.placeExit(exit);
+
+    huntAndKill(board);
+    drawBoardToCanvas(board);
+}
 
 function animateSolve(generator, board) {
     const crumbs = [];
@@ -64,22 +71,7 @@ function animateSolve(generator, board) {
 
         if (Array.isArray(next.value)) {
             const finalPath = next.value;
-            const pathIds = finalPath.map(c => c.toString());
-
-            crumbs.forEach(cell => {
-                if (pathIds.includes(cell.toString())) {
-                    const pathMarker = new Exit();
-                    pathMarker.cell = cell;
-                    cell.contents = pathMarker;
-                } else {
-                    delete cell.contents;
-                }
-            });
-
-            crumbs[0].contents = player;
-            crumbs[crumbs.length - 1].contents = exit;
-
-            drawBoardToCanvas(board);
+            drawFinalPath(finalPath, crumbs);
         } else {
             const cell = next.value;
             const crumb = new BreadCrumb();
@@ -90,10 +82,28 @@ function animateSolve(generator, board) {
 
             drawBoardToCanvas(board);
             if (!next.done) {
-                setTimeout(() => step(generator, board), 5);
+                setTimeout(() => step(), 0);
             }
         }
     }
 
     step();
+}
+
+function drawFinalPath(finalPath, crumbs) {
+    const pathIds = finalPath.map(c => c.toString());
+    crumbs.forEach(cell => {
+        if (pathIds.includes(cell.toString())) {
+            const pathMarker = new Exit();
+            pathMarker.cell = cell;
+            cell.contents = pathMarker;
+        } else {
+            delete cell.contents;
+        }
+    });
+
+    crumbs[0].contents = player;
+    crumbs[crumbs.length - 1].contents = exit;
+
+    drawBoardToCanvas(board);
 }
